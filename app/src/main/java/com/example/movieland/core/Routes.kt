455bad87462e8acd70dcc98b31f5467e3c787410
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-02 18:52:53 +0300

    feat: Routes sealed class added :sparkles:

---
+++ app/src/main/java/com/example/movieland/core/Routes.kt
@@ -0,0 +1,10 @@
+package com.example.movieland.core
+
+sealed class Routes(val route : String) {
+    data object Home : Routes("home_screen")
+    data object Latest : Routes("latest_screen")
+    data object Rated : Routes("rated_screen")
+    data object Favorite : Routes("favorite_screen")
+    data object Account : Routes("account_screen")
+    data object Detail : Routes("detail_screen")
+}
