18394773244479d00d4670735bb435d8dda661e8
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-02 18:50:50 +0300

    feat: Constants obj added :sparkles:

---
+++ app/src/main/java/com/example/movieland/core/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.movieland.core
+
+object Constants {
+
+    const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+    const val BEARER_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmRhOGYzY2MzZWUxNzQ3MDM5OTE4NzdiOWEwZWMzOSIsInN1YiI6IjY1NjBhMGJkMmIxMTNkMDEyZDAyNjA3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3nxtCPfSFgy8egn3HU1vg548cymyK5-s5mKta2KieGM"
+}
