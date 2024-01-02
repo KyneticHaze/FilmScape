509c5111a0131ddaa0db5d6bafdc5388bc7db765
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-02 14:29:30 +0300

    style: Material Theme color and shapes applied :gem:

---
+++ app/src/main/java/com/example/movieland/ui/theme/Shapes.kt
@@ -0,0 +1,13 @@
+package com.example.movieland.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    extraLarge = RoundedCornerShape(15.dp),
+    large = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(10.dp),
+    small = RoundedCornerShape(8.dp),
+    extraSmall = RoundedCornerShape(4.dp)
+)
