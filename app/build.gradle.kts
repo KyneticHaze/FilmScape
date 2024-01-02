d78e8d2cae458bb140172b7f57a9c1adc0f38360
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-02 10:17:02 +0300

    build: retrofit and hilt added :hammer_and_wrench:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,10 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.49")
     ksp("com.google.dagger:hilt-android-compiler:2.49")
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
