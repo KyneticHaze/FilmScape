44e9fd4ee60549281fc4ced0b92b67fde969ea7b
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-07 22:14:41 +0300

    style: color and Typography added :gem:

--- app/src/main/java/com/example/movieland/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieland/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -95,14 +96,6 @@ fun MovieLandTheme(
 
         darkTheme -> DarkColors
         else -> LightColors
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primaryContainer.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
     }
 
     MaterialTheme(
