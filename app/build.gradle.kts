c307944f6574d2f59c55ab78a804b6ebf382647e
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-07 22:02:10 +0300

    build: lottie lib added :hammer_and_wrench:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,9 @@ dependencies {
     // Coil
     implementation("io.coil-kt:coil-compose:2.5.0")
 
+    // Lottie
+    implementation("com.airbnb.android:lottie-compose:6.3.0")
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
