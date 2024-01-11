bd46278423e140b79a129eec7a245991f4d8abed
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-11 22:40:40 +0300

    feat: last image dto added :sparkles:

---
+++ app/src/main/java/com/example/movieland/data/remote/dto/image/ImageResponse.kt
@@ -0,0 +1,15 @@
+package com.example.movieland.data.remote.dto.image
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ImageResponse(
+    @SerializedName("backdrops")
+    val backdrops: List<Backdrop>?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("logos")
+    val logos: List<Logo>?,
+    @SerializedName("posters")
+    val posters: List<Poster>?
+)
