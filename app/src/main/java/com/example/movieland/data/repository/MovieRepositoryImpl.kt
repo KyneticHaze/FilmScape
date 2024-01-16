7f3a7be2c57de98ed33def5870d801183519024d
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-16 23:39:11 +0300

    feat: getMovieDetail func added :sparkles:

--- app/src/main/java/com/example/movieland/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/movieland/data/repository/MovieRepositoryImpl.kt
@@ -1,11 +1,12 @@
 package com.example.movieland.data.repository
 
 import com.example.movieland.data.remote.dto.credit.CreditResponse
-import com.example.movieland.data.remote.dto.detail.DetailResponse
+import com.example.movieland.data.remote.dto.detail.DetailDTO
 import com.example.movieland.data.remote.dto.commonDto.MovieResponse
+import com.example.movieland.data.remote.dto.image.ImageResponse
 import com.example.movieland.data.remote.dto.playing_upComing.PlayUpComingResponse
 import com.example.movieland.data.remote.dto.person.PersonResponse
-import com.example.movieland.data.service.ApiService
+import com.example.movieland.data.remote.service.ApiService
 import com.example.movieland.domain.repository.MovieRepository
 
 class MovieRepositoryImpl(
@@ -40,7 +41,7 @@ class MovieRepositoryImpl(
     ): PlayUpComingResponse =
         service.getNowPlayingMovies(lang, page, token)
 
-    override suspend fun getMovieDetail(movieId: Int, token: String): DetailResponse =
+    override suspend fun getMovieDetail(movieId: Int, token: String): DetailDTO =
         service.getMovieDetail(movieId, token)
 
     override suspend fun getMovieCredits(movieId: Int, token: String): CreditResponse =
@@ -48,4 +49,10 @@ class MovieRepositoryImpl(
 
     override suspend fun getPersonPopular(lang: String, page: Int, token: String): PersonResponse =
         service.getPersonPopular(lang, page, token)
+
+    override suspend fun getSearchMovie(query: String, page: Int, token: String): MovieResponse =
+        service.getSearchMovie(query, page, token)
+
+    override suspend fun getMovieImage(movieId: Int, page: Int, token: String): ImageResponse =
+        service.getMovieImage(movieId, page, token)
 }
--- app/src/main/java/com/example/movieland/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/example/movieland/domain/repository/MovieRepository.kt
@@ -1,8 +1,9 @@
 package com.example.movieland.domain.repository
 
 import com.example.movieland.data.remote.dto.credit.CreditResponse
-import com.example.movieland.data.remote.dto.detail.DetailResponse
+import com.example.movieland.data.remote.dto.detail.DetailDTO
 import com.example.movieland.data.remote.dto.commonDto.MovieResponse
+import com.example.movieland.data.remote.dto.image.ImageResponse
 import com.example.movieland.data.remote.dto.playing_upComing.PlayUpComingResponse
 import com.example.movieland.data.remote.dto.person.PersonResponse
 
@@ -13,7 +14,9 @@ interface MovieRepository {
     suspend fun getSimilarMovies(movieId: Int, lang: String, page: Int, token: String): MovieResponse
     suspend fun getUpComingMovies(lang: String, page: Int, token: String): PlayUpComingResponse
     suspend fun getNowPlayingMovies(lang: String, page: Int, token: String): PlayUpComingResponse
-    suspend fun getMovieDetail(movieId: Int, token: String): DetailResponse
+    suspend fun getMovieDetail(movieId: Int, token: String): DetailDTO
     suspend fun getMovieCredits(movieId: Int, token: String): CreditResponse
     suspend fun getPersonPopular(lang: String, page: Int, token: String): PersonResponse
+    suspend fun getSearchMovie(query : String, page: Int, token: String): MovieResponse
+    suspend fun getMovieImage(movieId: Int, page: Int, token: String): ImageResponse
 }
