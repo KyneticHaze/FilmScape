d6ed00bfa69470edb6c932f8a5a488d91d770501
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-19 20:38:29 +0300

    BREAKING_CHANGE: some files deleted :rotating_light:

--- app/src/main/java/com/example/movieland/core/DataStatus.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movieland.core
-
-sealed class DataStatus<out T> {
-    data class Success<out T>(val data: T) : DataStatus<T>()
-    data class Error(val errorMessage: String) : DataStatus<Nothing>()
-    data object Loading : DataStatus<Nothing>()
-}
--- app/src/main/java/com/example/movieland/ui/navigation/Routes.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movieland.ui.navigation
-
-sealed class Routes(val route : String) {
-    data object AppStart : Routes("appStart")
-    data object Auth : Routes("auth")
-    data object Home : Routes("home")
-}
