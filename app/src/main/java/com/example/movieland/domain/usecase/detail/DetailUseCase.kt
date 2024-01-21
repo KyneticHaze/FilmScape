9c5cc02ea2fa572a79282eaafb8703ba8b5000de
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-21 20:31:02 +0300

    BREAKING_CHANGE: usecases deleted :rotating_light:

--- app/src/main/java/com/example/movieland/domain/usecase/detail/DetailUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.movieland.domain.usecase.detail
-
-data class DetailUseCase(
-    val  detailMovieUseCase: DetailMovieUseCase,
-    val movieSimilarUseCase: MovieSimilarUseCase,
-    val movieImageUseCase: MovieImageUseCase,
-    val movieCreditsUseCase: MovieCreditsUseCase
-)
--- app/src/main/java/com/example/movieland/ui/screens/home_screen/util/ImagePath.kt
+++
@@ -1,5 +0,0 @@
-package com.example.movieland.ui.screens.home_screen.util
-
-fun imagePath(path: String): String {
-    return "https://image.tmdb.org/t/p/w500$path"
-}
