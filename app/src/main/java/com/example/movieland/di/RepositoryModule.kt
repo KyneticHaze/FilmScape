7013db083b7beb1d0d4602351d1b6cb7d825d3e3
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-21 20:32:01 +0300

    refactor: usecase di deleted :package:

--- app/src/main/java/com/example/movieland/di/RepositoryModule.kt
+++ app/src/main/java/com/example/movieland/di/RepositoryModule.kt
@@ -1,19 +1,8 @@
 package com.example.movieland.di
 
-import com.example.movieland.data.repository.MovieRepositoryImpl
-import com.example.movieland.data.remote.service.ApiService
-import com.example.movieland.domain.repository.MovieRepository
-import com.example.movieland.domain.usecase.detail.DetailMovieUseCase
-import com.example.movieland.domain.usecase.detail.DetailUseCase
-import com.example.movieland.domain.usecase.detail.MovieCreditsUseCase
-import com.example.movieland.domain.usecase.detail.MovieImageUseCase
-import com.example.movieland.domain.usecase.detail.MovieSimilarUseCase
-import com.example.movieland.domain.usecase.main.MainUseCase
-import com.example.movieland.domain.usecase.main.NowPlayingUseCase
-import com.example.movieland.domain.usecase.main.PopularUseCase
-import com.example.movieland.domain.usecase.main.SearchMovieUseCase
-import com.example.movieland.domain.usecase.main.TopRatedUseCase
-import com.example.movieland.domain.usecase.main.UpComingUseCase
+import com.example.movieland.data.repository.MediaRepositoryImpl
+import com.example.movieland.data.remote.api.MediaApi
+import com.example.movieland.domain.repository.MediaRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,34 +16,8 @@ object RepositoryModule {
     @Provides
     @Singleton
     fun provideRepository(
-        service: ApiService
-    ): MovieRepository {
-        return MovieRepositoryImpl(service)
-    }
-
-    @Provides
-    @Singleton
-    fun provideMainUseCase(
-        repository : MovieRepository
-    ) : MainUseCase {
-        return MainUseCase(
-            nowPlayingUseCase = NowPlayingUseCase(repository),
-            upComingUseCase = UpComingUseCase(repository),
-            popularUseCase = PopularUseCase(repository),
-            topRatedUseCase = TopRatedUseCase(repository),
-            searchMovieUseCase = SearchMovieUseCase(repository)
-        )
-    }
-    @Provides
-    @Singleton
-    fun provideDetailUseCase(
-        repository : MovieRepository
-    ) : DetailUseCase {
-        return DetailUseCase(
-            movieImageUseCase = MovieImageUseCase(repository),
-            movieSimilarUseCase = MovieSimilarUseCase(repository),
-            detailMovieUseCase = DetailMovieUseCase(repository),
-            movieCreditsUseCase = MovieCreditsUseCase(repository)
-        )
+        api: MediaApi
+    ): MediaRepository {
+        return MediaRepositoryImpl(api)
     }
 }
