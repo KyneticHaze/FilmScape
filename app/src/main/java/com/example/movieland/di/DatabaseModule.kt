432018af2ebf44f4f1d8ed851ae3e2933a18a50f
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-24 18:06:37 +0300

    feat: db module added :sparkles:

---
+++ app/src/main/java/com/example/movieland/di/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package com.example.movieland.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.example.movieland.core.Constants.MEDIA_DB
+import com.example.movieland.data.local.MediaDB
+import com.example.movieland.data.local.MediaDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideMediaDatabase(@ApplicationContext context: Context): MediaDB {
+        return Room.databaseBuilder(
+            context,
+            MediaDB::class.java,
+            MEDIA_DB
+        ).build()
+    }
+}
