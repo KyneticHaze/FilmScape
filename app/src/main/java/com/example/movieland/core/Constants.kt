15c824bf2b873bfd3c36a902fb9f8f2807eeea74
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-24 18:05:48 +0300

    feat: Constants added :sparkles:

---
+++ app/src/main/java/com/example/movieland/core/Constants.kt
@@ -0,0 +1,17 @@
+package com.example.movieland.core
+
+object Constants {
+
+    const val MEDIA_DB = "media_db"
+
+    const val MOVIE = "movie"
+    const val TV = "tv"
+    const val TRENDING_TIME = "day"
+
+    const val NOW_PLAYING = "now_playing"
+    const val TOP_RATED = "top_rated"
+    const val POPULAR = "popular"
+    const val TRENDING = "trending"
+
+    const val unavailable = "Unavailable"
+}
