0c9b81e0bf689b380c50b84dc223133f09a52601
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-25 23:56:51 +0300

    feat: screens constants added :sparkles:

--- app/src/main/java/com/example/movieland/core/Constants.kt
+++ app/src/main/java/com/example/movieland/core/Constants.kt
@@ -7,6 +7,7 @@ object Constants {
     const val MOVIE = "movie"
     const val TV = "tv"
     const val TRENDING_TIME = "day"
+    const val ALL = "all"
 
     const val NOW_PLAYING = "now_playing"
     const val TOP_RATED = "top_rated"
@@ -14,4 +15,12 @@ object Constants {
     const val TRENDING = "trending"
 
     const val unavailable = "Unavailable"
+
+    // Screens
+    const val HOME_SCREEN = "home_screen"
+    const val TV_SCREEN = "tv_screen"
+    const val POPULAR_SCREEN = "popular_screen"
+    const val TOP_RATED_SCREEN = "top_rated_screen"
+    const val TRENDING_SCREEN = "trending_screen"
+    const val SEARCH_SCREEN = "search_screen"
 }
