84cc68de8dcd1734fa74ef61be82d1cd4f5b0696
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-25 23:57:34 +0300

    refactor: room func added and usecase deleted :package:

--- app/src/main/java/com/example/movieland/data/repository/MediaRepositoryImpl.kt
+++ app/src/main/java/com/example/movieland/data/repository/MediaRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.example.movieland.data.repository
 
+import com.example.movieland.core.Constants.TRENDING
 import com.example.movieland.core.Resource
+import com.example.movieland.data.local.MediaDao
 import com.example.movieland.data.mappers.toMedia
+import com.example.movieland.data.mappers.toMediaEntity
 import com.example.movieland.data.remote.api.MediaApi
 import com.example.movieland.domain.model.Media
 import com.example.movieland.domain.repository.MediaRepository
@@ -11,9 +14,27 @@ import retrofit2.HttpException
 import java.io.IOException
 
 class MediaRepositoryImpl(
-    private val api: MediaApi
+    private val api: MediaApi,
+    private val dao: MediaDao
 ) : MediaRepository {
+
+    override suspend fun getMedia(id: Int, type: String, category: String): Media {
+        return dao.getMediaById(id).toMedia(type, category)
+    }
+
+    override suspend fun insertMedia(media: Media) {
+        val mediaEntity = media.toMediaEntity()
+        return dao.insertMedia(mediaEntity)
+    }
+
+    override suspend fun updateMedia(media: Media) {
+        val mediaEntity = media.toMediaEntity()
+        return dao.updateMedia(mediaEntity)
+    }
+
     override suspend fun getAnythings(
+        fetchFromRemote: Boolean,
+        isRefresh: Boolean,
         type: String,
         category: String,
         page: Int,
@@ -22,8 +43,33 @@ class MediaRepositoryImpl(
         return flow {
             emit(Resource.Loading(isLoading = true))
 
+            val localMedias = dao.getMediasByTypeAndCategory(type, category)
+
+            // verileri tabandan çekmek için
+            val shouldLoadFromCache =
+            /// local medias boş olmayacak
+                /// uzaktan veri çekmemek ve yenilememek şartı ile
+                localMedias.isNotEmpty() && !fetchFromRemote && !isRefresh
+
+            if (shouldLoadFromCache) {
+                emit(Resource.Success(
+                    data = localMedias.map { it.toMedia(type, category) }
+                ))
+                emit(Resource.Loading(true))
+                return@flow
+            }
+
+            var searchPage = page
+            if (isRefresh) {
+                /// Sayfa yenilendiğinde media verilerini sil ve arama sayfasını 1'e endekle
+                dao.deleteMediaByTypeAndCategory(type, category)
+                searchPage = 1
+            }
+
             val mediaList = try {
-                api.getAnythings(type, category, page, apiKey).medias
+                /// 1'e endekslenen sayfayı veri çekme fonksiyonunda yerleştir.
+                /// sayfa yenilenirse 1 yenilenmez ise verilen sayfadan veri çeker.
+                api.getAnythings(type, category, searchPage, apiKey).medias
             } catch (e: IOException) {
                 emit(Resource.Error(errorMessage = e.localizedMessage ?: "Couldn't load data!"))
                 emit(Resource.Loading(isLoading = false))
@@ -35,16 +81,19 @@ class MediaRepositoryImpl(
             }
 
             mediaList?.let {
-                val media = mediaList.map {
-                    it.toMedia()
-                }
-                emit(Resource.Success(data = media))
+                val remoteMedia = mediaList.map { it.toMedia(type, category) }
+                val entities = mediaList.map { it.toMediaEntity(type, category) }
+
+                dao.insertMedias(entities)
+                emit(Resource.Success(data = remoteMedia))
                 emit(Resource.Loading(isLoading = false))
             }
         }
     }
 
     override suspend fun getTrendingAnything(
+        fetchFromRemote: Boolean,
+        isRefresh: Boolean,
         type: String,
         time: String,
         page: Int,
@@ -53,8 +102,26 @@ class MediaRepositoryImpl(
         return flow {
             emit(Resource.Loading(true))
 
+            val localMedias = dao.getTrendingMedias(TRENDING)
+
+            val shouldLoadCache =
+                localMedias.isNotEmpty() && !fetchFromRemote && !isRefresh
+
+            if (shouldLoadCache) {
+                emit(Resource.Success(data = localMedias.map { it.toMedia(type, TRENDING) }))
+                emit(Resource.Loading(true))
+                return@flow
+            }
+
+            var searchPage = page
+
+            if (isRefresh) {
+                dao.deleteTrendingMedia(TRENDING)
+                searchPage = 1
+            }
+
             val mediaList = try {
-                api.getTrendingAnything(type, time, page, apiKey).medias
+                api.getTrendingAnything(type, time, searchPage, apiKey).medias
             } catch (e: IOException) {
                 emit(Resource.Error(e.localizedMessage ?: "Couldn't load data!"))
                 emit(Resource.Loading(false))
@@ -66,9 +133,9 @@ class MediaRepositoryImpl(
             }
 
             mediaList?.let {
-                val media = mediaList.map {
-                    it.toMedia()
-                }
+                val media = mediaList.map { it.toMedia(type, TRENDING) }
+                val entities = mediaList.map { it.toMediaEntity(type, TRENDING) }
+                dao.insertMedias(entities)
                 emit(Resource.Success(data = media))
                 emit(Resource.Loading(false))
             }
@@ -76,6 +143,7 @@ class MediaRepositoryImpl(
     }
 
     override suspend fun getSearchAnything(
+        fetchFromRemote: Boolean,
         query: String,
         page: Int,
         apiKey: String
@@ -99,7 +167,10 @@ class MediaRepositoryImpl(
 
             mediaListSearch?.let {
                 val mediaToSearch = it.map { media ->
-                    media.toMedia()
+                    media.toMedia(
+                        type = media.mediaType,
+                        category = media.category
+                    )
                 }
 
                 emit(Resource.Success(data = mediaToSearch))
