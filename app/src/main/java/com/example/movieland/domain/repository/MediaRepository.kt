2c25b6cde80a38766825d4415552954e2ceddd1d
Furkan Harmancı <rocklee34@hotmail.com>
2024-01-25 23:57:08 +0300

    feat: room func added :sparkles:

--- app/src/main/java/com/example/movieland/domain/repository/MediaRepository.kt
+++ app/src/main/java/com/example/movieland/domain/repository/MediaRepository.kt
@@ -5,7 +5,24 @@ import com.example.movieland.domain.model.Media
 import kotlinx.coroutines.flow.Flow
 
 interface MediaRepository {
+
+    // Room functions
+
+    suspend fun getMedia(
+        id: Int,
+        type: String,
+        category: String
+    ): Media
+
+    suspend fun insertMedia(media: Media)
+
+    suspend fun updateMedia(media: Media)
+
+
+    // Retrofit functions
     suspend fun getAnythings(
+        fetchFromRemote: Boolean,
+        isRefresh: Boolean,
         type: String,
         category: String,
         page: Int,
@@ -13,6 +30,8 @@ interface MediaRepository {
     ): Flow<Resource<List<Media>>>
 
     suspend fun getTrendingAnything(
+        fetchFromRemote: Boolean,
+        isRefresh: Boolean,
         type: String,
         time: String,
         page: Int,
@@ -20,6 +39,7 @@ interface MediaRepository {
     ): Flow<Resource<List<Media>>>
 
     suspend fun getSearchAnything(
+        fetchFromRemote: Boolean,
         query: String,
         page: Int,
         apiKey: String
