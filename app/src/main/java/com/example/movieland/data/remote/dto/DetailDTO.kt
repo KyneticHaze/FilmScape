4f8da93c827b6e7c202515ab1807ec8c8cc401ff
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-02 18:45:34 +0300

    refactor: DetailDTO nullable var deleted :package:

--- app/src/main/java/com/example/movieland/data/remote/dto/DetailDTO.kt
+++ app/src/main/java/com/example/movieland/data/remote/dto/DetailDTO.kt
@@ -1,8 +1,8 @@
 package com.example.movieland.data.remote.dto
 
 data class DetailDTO(
-    val revenue : Int?,
-    val runtime: Int?,
-    val status: String?,
-    val tagline: String?
+    val revenue : Int,
+    val runtime: Int,
+    val status: String,
+    val tagline: String
 )
