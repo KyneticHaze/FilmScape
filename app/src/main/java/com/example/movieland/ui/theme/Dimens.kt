d70c112a46efea4efb03c40212c41ef680db84be
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-05 12:25:14 +0300

    feat: radius object added :sparkles:

---
+++ app/src/main/java/com/example/movieland/ui/theme/Dimens.kt
@@ -0,0 +1,9 @@
+package com.example.movieland.ui.theme
+
+object Dimens {
+    const val SMALL_RADIUS = 8
+    const val MEDIUM_RADIUS = 16
+    const val LARGE_RADIUS = 20
+    const val HUGE_RADIUS = 74
+    const val RADIUS_CONTAINER = 24
+}
