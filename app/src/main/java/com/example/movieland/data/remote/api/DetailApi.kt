0bc2c8364cec3daba0f85bfbbcb74a683fb35b9b
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-08 23:07:38 +0300

    refactor: movie word changed to media :package:

--- app/src/main/java/com/example/movieland/data/remote/api/DetailApi.kt
+++ app/src/main/java/com/example/movieland/data/remote/api/DetailApi.kt
@@ -1,29 +1,30 @@
 package com.example.movieland.data.remote.api
 
 import com.example.movieland.data.remote.dto.DetailDTO
-import com.example.movieland.data.remote.dto.movie.MediaResponse
+import com.example.movieland.data.remote.dto.media.MediaResponse
 import com.example.movieland.data.remote.dto.image.ImageResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface DetailApi {
-    @GET("{type}/{movie_id}")
-    suspend fun getMovieDetail(
+    @GET("{type}/{media_id}")
+    suspend fun getMediaDetails(
         @Path("type") type: String,
-        @Path("movie_id") id: String,
+        @Path("media_id") id: Int,
         @Query("api_key") apiKey: String
     ): DetailDTO
 
-    @GET("movie/{movie_id}/similar")
-    suspend fun getSimilarMovies(
-        @Path("movie_id") movieId : Int,
+    @GET("{type}/{media_id}/similar")
+    suspend fun getSimilarMedias(
+        @Path("type") type: String,
+        @Path("media_id") mediaId : Int,
         @Query("page") page: Int,
         @Query("api_key") apiKey: String
     ): MediaResponse
 
     @GET("{type}/{type_id}/images")
-    suspend fun getMovieImage(
+    suspend fun getMediaImages(
         @Path("type") type: String,
         @Path("type_id") typeId: Int,
         @Query("api_key") token: String
