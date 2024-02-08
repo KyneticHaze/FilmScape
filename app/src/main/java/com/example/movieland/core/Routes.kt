37301c238a606beb2188167ca26a9dff174af8ea
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-08 23:07:14 +0300

    refactor: other routes deleted :package:

--- app/src/main/java/com/example/movieland/core/Routes.kt
+++ app/src/main/java/com/example/movieland/core/Routes.kt
@@ -4,9 +4,5 @@ sealed class Routes(val route : String) {
     data object Splash : Routes("splash_screen")
     data object Main : Routes("main_screen")
     data object Detail : Routes("detail_screen")
-    data object Upcoming : Routes("upcoming_screen")
-    data object TopRated : Routes("top_rated_screen")
-    data object Playing  : Routes("playing_screen")
-    data object Popular : Routes("popular_screen")
     data object Search : Routes("search_screen")
 }
