fc9c417df1a8055d20c664d0c04398ff9b72b30a
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-09 23:14:34 +0300

    refactor: runtime, status, tagline deleted :package:

--- app/src/main/java/com/example/movieland/data/mappers/MediaMapper.kt
+++ app/src/main/java/com/example/movieland/data/mappers/MediaMapper.kt
@@ -1,10 +1,12 @@
 package com.example.movieland.data.mappers
 
 import com.example.movieland.core.Constants
-import com.example.movieland.data.remote.dto.movie.MediaDTO
+import com.example.movieland.data.local.MediaEntity
+import com.example.movieland.data.remote.dto.media.MediaDTO
 import com.example.movieland.domain.model.Media
 
-fun MediaDTO.toMedia() : Media {
+fun MediaDTO.toMedia(
+): Media {
     return Media(
         adult = adult ?: false,
         backdropPath = backdropPath ?: Constants.unavailable,
@@ -19,8 +21,65 @@ fun MediaDTO.toMedia() : Media {
         title = title ?: Constants.unavailable,
         voteAverage = voteAverage ?: 0.0,
         voteCount = voteCount ?: 0,
-        runtime = null,
-        status = null,
-        tagline = null
+        video = video ?: false
+    )
+}
+
+fun MediaEntity.toMedia(): Media {
+    return Media(
+        adult = adult,
+        backdropPath = backdropPath,
+        originalLanguage = originalLanguage,
+        overview = overview,
+        posterPath = posterPath,
+        releaseDate = releaseDate,
+        title = title,
+        voteAverage = voteAverage,
+        popularity = popularity,
+        voteCount = voteCount,
+        genreIds = genreIds.split(",").map { it.toInt() }, // "35,80"
+        id = id,
+        originalTitle = originalTitle,
+        video = video,
+    )
+}
+
+fun MediaDTO.toMediaEntity(): MediaEntity {
+    return MediaEntity(
+        adult = adult ?: false,
+        backdropPath = backdropPath ?: Constants.unavailable,
+        id = id ?: 1,
+        overview = overview ?: Constants.unavailable,
+        originalLanguage = originalLanguage ?: Constants.unavailable,
+        originalTitle = originalTitle ?: Constants.unavailable,
+        title = title ?: Constants.unavailable,
+        releaseDate = releaseDate ?: Constants.unavailable,
+        posterPath = posterPath ?: Constants.unavailable,
+        video = video ?: false,
+        voteAverage = voteAverage ?: 0.0,
+        voteCount = voteCount ?: 0,
+        firstAirDate = releaseDate ?: Constants.unavailable,
+        popularity = popularity ?: 0.0,
+        genreIds = genreIds?.joinToString(",") ?: "-1,-2"
+    )
+}
+
+fun Media.toMediaEntity(): MediaEntity {
+    return MediaEntity(
+        backdropPath = backdropPath,
+        adult = adult,
+        originalTitle = originalTitle,
+        originalLanguage = originalLanguage,
+        releaseDate = releaseDate,
+        title = title,
+        voteCount = voteCount,
+        voteAverage = voteAverage,
+        id = id,
+        firstAirDate = releaseDate,
+        genreIds = genreIds.joinToString(","),
+        overview = overview,
+        posterPath = posterPath,
+        popularity = popularity,
+        video = video
     )
 }
