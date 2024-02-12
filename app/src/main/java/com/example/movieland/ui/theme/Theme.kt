8a91a4bdc58f2994ee63256730b62a7f823cbbd1
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-12 22:02:15 +0300

    refactor: shape class deleted and Shapes material class added :package:

--- app/src/main/java/com/example/movieland/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieland/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -100,7 +101,7 @@ fun MovieLandTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        shapes = shapes,
+        shapes = Shapes(),
         typography = Typography,
         content = content
     )
