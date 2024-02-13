2e4bc00a84798463628001df8a2210ddeae711d1
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-13 22:22:38 +0300

    refactor: kdoc added :package:

--- app/src/main/java/com/example/movieland/data/local/MediaDB.kt
+++ app/src/main/java/com/example/movieland/data/local/MediaDB.kt
@@ -3,6 +3,10 @@ package com.example.movieland.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
+/**
+ * @author Furkan Harmancı
+ * Media veritabanı
+ */
 @Database(
     entities = [MediaEntity::class],
     version = 1
