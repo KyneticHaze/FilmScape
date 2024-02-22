0f0ebfb45bd452a23d015ec3a70c2862526a3c15
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-22 13:17:43 +0300

    refactor: Route screen modified :package:

--- app/src/main/java/com/example/movieland/core/Routes.kt
+++ app/src/main/java/com/example/movieland/core/Routes.kt
@@ -1,8 +1,9 @@
 package com.example.movieland.core
 
 sealed class Routes(val route : String) {
-    data object Splash : Routes("splash_screen")
-    data object Main : Routes("main_screen")
+    data object Home : Routes("home_screen")
+    data object Person: Routes("person_screen")
+    data object Favorites: Routes("favorites_screen")
     data object Detail : Routes("detail_screen")
     data object Search : Routes("search_screen")
 }
