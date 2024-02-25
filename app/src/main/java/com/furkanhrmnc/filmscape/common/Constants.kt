0842dc574a8ddbacc35235997bd51376b60fb130
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-26 00:27:28 +0300

    feat: room table vars added :sparkles:

--- app/src/main/java/com/furkanhrmnc/filmscape/common/Constants.kt
+++ app/src/main/java/com/furkanhrmnc/filmscape/common/Constants.kt
@@ -2,29 +2,35 @@ package com.furkanhrmnc.filmscape.common
 
 object Constants {
 
-    const val MEDIA_DB = "media_db"
+    // Veritabanı sabitleri
+    const val REMOTE_KEYS_TABLE = "remote_keys_table"
+    const val MEDIA_TABLE = "media_table"
+    const val DETAIL_TABLE = "detail_table"
+    const val CREATED_BY_TABLE = "created_by_table"
+    const val SEASON_TABLE = "season_table"
+    const val FILM_SCAPE_DB = "film_scape_db"
 
+    // Media types
     const val MOVIE = "movie"
     const val TV = "tv"
 
+    // Trending types
+    const val TRENDING = "trending"
     const val DAY = "day"
     const val WEEK = "week"
-
     const val ALL = "all"
 
+    // Movie types
     const val NOW_PLAYING = "now_playing"
     const val TOP_RATED = "top_rated"
     const val POPULAR = "popular"
-    const val TRENDING = "trending"
 
+    // Empty string
     const val unavailable = "Unavailable"
 
     // Screens
     const val HOME_SCREEN = "home_screen"
-    const val MOVIE_SCREEN = "movie_screen"
+    const val MOVIES_SCREEN = "movies_screen"
     const val TV_SCREEN = "tv_screen"
-    const val POPULAR_SCREEN = "popular_screen"
-    const val TOP_RATED_SCREEN = "top_rated_screen"
     const val TRENDING_SCREEN = "trending_screen"
-    const val SEARCH_SCREEN = "search_screen"
 }
