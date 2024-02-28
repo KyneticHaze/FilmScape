fc5608f51d5b529f029509524b4a1e0de0a12c2a
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-28 19:36:24 +0300

    refactor: releaseDate changed :package:

--- app/src/main/java/com/furkanhrmnc/filmscape/domain/model/Media.kt
+++ app/src/main/java/com/furkanhrmnc/filmscape/domain/model/Media.kt
@@ -1,20 +1,22 @@
 package com.furkanhrmnc.filmscape.domain.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
-
 @Parcelize
 data class Media(
+    val id: Int,
     val adult: Boolean,
     val backdropPath: String,
     val genreIds: List<Int>,
-    val id: Int,
     val originalLanguage: String,
     val originalTitle: String,
     val overview: String,
     val popularity: Double,
     val posterPath: String,
-    val releaseDate: String,
+    val movieDate: String,
+    val tvDate: String,
     val title: String,
     val voteAverage: Double,
     val video: Boolean,
