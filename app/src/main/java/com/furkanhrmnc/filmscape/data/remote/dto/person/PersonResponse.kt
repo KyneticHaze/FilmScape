a15f739311511ce764d3f520ba9050334d22c457
Furkan Harmancı <rocklee34@hotmail.com>
2024-02-28 19:36:41 +0300

    feat: person dto added :sparkles:

---
+++ app/src/main/java/com/furkanhrmnc/filmscape/data/remote/dto/person/PersonResponse.kt
@@ -0,0 +1,15 @@
+package com.furkanhrmnc.filmscape.data.remote.dto.person
+
+
+import com.google.gson.annotations.SerializedName
+
+data class PersonResponse(
+    @SerializedName("page")
+    val page: Int?,
+    @SerializedName("results")
+    val persons: List<PersonDTO>?,
+    @SerializedName("total_pages")
+    val totalPages: Int?,
+    @SerializedName("total_results")
+    val totalResults: Int?
+)
