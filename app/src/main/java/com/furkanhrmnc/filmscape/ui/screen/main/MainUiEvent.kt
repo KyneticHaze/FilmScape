610bfdc9ddacc34567834263eafea59e6a1d3e50
Furkan Harmancı <rocklee34@hotmail.com>
2024-03-01 20:46:13 +0300

    BREAKING_CHANGE: other dtos deleted :rotating_light:

--- app/src/main/java/com/furkanhrmnc/filmscape/ui/screen/main/MainUiEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.furkanhrmnc.filmscape.ui.screen.main
-
-sealed class MainUiEvent {
-    data class Paginate(val type : String): MainUiEvent()
-    data class Refresh(val type : String): MainUiEvent()
-}
