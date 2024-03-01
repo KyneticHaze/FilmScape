bc8872b04543fc9553ca67c9498dbcf35c894306
Furkan Harmancı <rocklee34@hotmail.com>
2024-03-01 20:46:32 +0300

    feat: BottomBar added :sparkles:

--- app/src/main/java/com/furkanhrmnc/filmscape/ui/util/BottomBar.kt
+++ app/src/main/java/com/furkanhrmnc/filmscape/ui/util/BottomBar.kt
@@ -1,11 +1,45 @@
 package com.furkanhrmnc.filmscape.ui.util
 
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.navigation.NavController
+import com.furkanhrmnc.filmscape.ui.navigation.NavBarItem
 
 @Composable
-fun BottomBar() {
-    NavigationBar {
+fun BottomBar(
+    navController: NavController
+) {
 
+    val navList = listOf(
+        NavBarItem.Home,
+        NavBarItem.Person,
+        NavBarItem.Favorite
+    )
+
+    var selectedItemIndex by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
+    NavigationBar {
+        navList.forEachIndexed { index, navBarItem ->
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+                    navController.navigate(navBarItem.route)
+                },
+                icon = {
+                    Icon(
+                        imageVector = if (selectedItemIndex == index) navBarItem.selectedIcon else navBarItem.unselectedIcon,
+                        contentDescription = navBarItem.title
+                    )
+                }
+            )
+        }
     }
 }
