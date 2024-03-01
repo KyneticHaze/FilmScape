0c62867f6584531daf1f7f6c00ec11044b76af3e
Furkan Harmancı <rocklee34@hotmail.com>
2024-03-01 20:46:49 +0300

    feat: tv types added :sparkles:

--- app/src/main/java/com/furkanhrmnc/filmscape/common/Constants.kt
+++ app/src/main/java/com/furkanhrmnc/filmscape/common/Constants.kt
@@ -3,14 +3,8 @@ package com.furkanhrmnc.filmscape.common
 object Constants {
 
     // Veritabanı sabitleri
-    const val REMOTE_KEYS_TABLE = "remote_keys_table"
     const val MEDIA_TABLE = "media_table"
-    const val DETAIL_TABLE = "detail_table"
-    const val CREATED_BY_TABLE = "created_by_table"
-    const val SEASON_TABLE = "season_table"
     const val FILM_SCAPE_DB = "film_scape_db"
-
-    const val PER_ITEM_PAGE = 20
 
     // Media types
     const val MOVIE = "movie"
@@ -20,12 +14,16 @@ object Constants {
     const val TRENDING = "trending"
     const val DAY = "day"
     const val WEEK = "week"
-    const val ALL = "all"
 
     // Movie types
     const val NOW_PLAYING = "now_playing"
     const val TOP_RATED = "top_rated"
     const val POPULAR = "popular"
+    const val UPCOMING = "upcoming"
+
+    // Tv types
+    const val ON_THE_AIR = "on_the_air"
+    const val AIRING_TODAY = "airing_today"
 
     // Empty string
     const val unavailable = "Unavailable"
