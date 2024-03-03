d33f6f47d0e8caee1af678b0735b8277123734c1
Furkan Harmancı <rocklee34@hotmail.com>
2024-03-03 19:39:53 +0300

    refactor: DetailEntity deleted :package:

--- app/src/main/java/com/furkanhrmnc/filmscape/data/local/FilmScapeDB.kt
+++ app/src/main/java/com/furkanhrmnc/filmscape/data/local/FilmScapeDB.kt
@@ -2,24 +2,21 @@ package com.furkanhrmnc.filmscape.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.furkanhrmnc.filmscape.data.local.detail.DetailDao
-import com.furkanhrmnc.filmscape.data.local.detail.DetailEntity
 import com.furkanhrmnc.filmscape.data.local.media.MediaDao
 import com.furkanhrmnc.filmscape.data.local.media.MediaEntity
 
 /**
- * Projenin veritabanı soyut sınıfıdır.
+ * Projenin soyut veritabanı sınıfıdır.
  *
  * @property MediaEntity
- * @property DetailEntity
  *
  * @author Furkan Harmancı
  */
 @Database(
-    entities = [MediaEntity::class, DetailEntity::class],
-    version = 1
+    entities = [MediaEntity::class],
+    version = 1,
+    exportSchema = false
 )
 abstract class FilmScapeDB: RoomDatabase() {
     abstract fun mediaDao(): MediaDao
-    abstract fun detailDao(): DetailDao
 }
