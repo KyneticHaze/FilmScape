48afa5571e75844b379b8d9f683e71e0d508b933
Furkan Harmancı <rocklee34@hotmail.com>
2024-03-05 18:41:33 +0300

    refactor: all changed refactored :package:

--- app/src/main/java/com/furkanhrmnc/filmscape/ui/screen/detail/DetailEvents.kt
+++
@@ -1,7 +0,0 @@
-package com.furkanhrmnc.filmscape.ui.screen.detail
-
-sealed class DetailEvents {
-    data object Refresh: DetailEvents()
-
-    data class SetDataOnLoad(val id: Int): DetailEvents()
-}
