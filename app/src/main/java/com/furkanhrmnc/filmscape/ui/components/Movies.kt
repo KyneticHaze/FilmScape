711e5e0f075f2150e96d8dc002ecb29a59e36462
Furkan Harmancı <rocklee34@hotmail.com>
2024-04-25 18:52:21 +0300

    feat: other components deleted :sparkles:

---
+++ app/src/main/java/com/furkanhrmnc/filmscape/ui/components/Movies.kt
@@ -0,0 +1,67 @@
+package com.furkanhrmnc.filmscape.ui.components
+
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import com.furkanhrmnc.filmscape.domain.model.Movie
+import com.furkanhrmnc.filmscape.domain.usecase.ViewState
+import com.furkanhrmnc.filmscape.util.ApiConfig
+
+@Composable
+fun Movies(
+    modifier: Modifier = Modifier,
+    moviesState: ViewState<List<Movie>>,
+    onMovieClick: (movie: Movie) -> Unit
+) {
+    when (moviesState) {
+        is ViewState.Failure -> {
+            moviesState.exception?.message?.run {
+                Text(text = this)
+            }
+        }
+
+        ViewState.Loading -> {
+            CircularProgressIndicator()
+        }
+
+        is ViewState.Success -> {
+            if (moviesState.data.isNotEmpty()) {
+                LazyRow(
+                    modifier = modifier
+                ) {
+                    items(
+                        items = moviesState.data,
+                        key = { movie -> movie.id }
+                    ) { movie ->
+                        val context = LocalContext.current
+                        val imageUrl = "${ApiConfig.IMAGE_URL}${movie.posterPath}"
+                        val painter = rememberAsyncImagePainter(
+                            model = ImageRequest.Builder(context)
+                                .crossfade(1000)
+                                .size(Size.ORIGINAL)
+                                .data(imageUrl)
+                                .build()
+                        )
+                        MovieCard(
+                            painter = painter,
+                            title = movie.title,
+                            onClick = { onMovieClick(movie) },
+                            background = MaterialTheme.colorScheme.background,
+                            content = MaterialTheme.colorScheme.onBackground,
+                            elevation = 2.dp
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
