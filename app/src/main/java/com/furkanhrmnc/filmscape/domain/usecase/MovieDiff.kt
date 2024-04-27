03c303fb487e64c6f856f3e74b209c9e31b79ff0
Furkan Harmancı <rocklee34@hotmail.com>
2024-04-27 17:48:23 +0300

    refactor: name changed :package:

--- app/src/main/java/com/furkanhrmnc/filmscape/domain/usecase/MovieDiff.kt
+++ app/src/main/java/com/furkanhrmnc/filmscape/domain/usecase/MovieDiff.kt
@@ -2,7 +2,7 @@ package com.furkanhrmnc.filmscape.domain.usecase
 
 import com.furkanhrmnc.filmscape.util.Category
 
-data class Param(
+data class MovieDiff(
     val category: Category,
     val page: Int = 1
 )
